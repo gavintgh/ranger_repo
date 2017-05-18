b0634a4ae6f41dbd488911786e784b2c816f0106
gavintgh <gavintgh@users.noreply.github.com>
2017-05-18 21:46:38 +0800

    add extract ranger hdfs/hive policy script
    add extract ranger hdfs/hive policy script

---
+++ extract_ranger_hive_policy.py
@@ -0,0 +1,51 @@
+import json
+
+input_file=open('/tmp/gavin/gavin.hdfs.out')
+output_file=open('/tmp/gavin/ranger.hdfs.out')
+
+policy_template = {
+   'name': '',
+   'policyItems': [],
+   'database': []
+   }
+
+item = json.load(input_file)
+
+result = item['policies']
+
+for i in range(len(result)):
+   policy = policy_template
+   policy['name'] = result[i]['name']
+   
+   policy['policyItems'] = [{
+      'users':[],
+	  'groups':[],
+	  'accesses':[]
+   }]
+   policy['database'] = [{
+      'values':[]
+   }]
+   if result[i]['policyItems']:
+      for k in range(len(result[i]['policyItems'])):
+	     if result[i]['policyItems'][k]['groups']:
+		    for j in range(len(result[i]['policyItems'][k]['groups'])):
+			   policy['policyItems'][0]['groups'].append(result[i]['policyItems'][k]['groups'][j])
+		 if result[i]['policyItems'][k]['users']:
+		    for j in range(len(result[i]['policyItems'][k]['users'])):
+			   policy['policyItems'][0]['users'].append(result[i]['policyItems'][k]['users'][j])
+		 if result[i]['policyItems'][k]['accesses']:
+		    for j in range(len(result[i]['policyItems'][k]['accesses'])):
+			   policy['policyItems'][0]['accesses'].append(result[i]['policyItems'][k]['accesses'][j]['type'])
+		 	   
+   if result[i]['resources']:
+      if result[i]['resources']['database']:
+	     for k in range(len(result[i]['resources']['database']['values'])):
+		    policy['database'][0]['values'].append(result[i]['resources']['database']['values'][j])
+	
+	back_json = json.dumps(policy,output_file)
+	output_file.write(back_json + '\n')
+	
+input_file.close()
+output_file.close()
+
+   
